afca13b6c498d6800ac8fad96ccf7cd724f3a3d2
Julia Bower <julia.bower@serko.com>
2023-08-03 10:20:13 +1200

    remove unused items, update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,10 @@ import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 import '../styles/globals.css';
 
-const title = 'Twitter Bio Generator';
-const description = 'Generate your next Twitter bio in seconds.';
+const title = 'Storytime app';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://twitterbio.io'),
   title,
-  description,
-  openGraph: {
-    title,
-    description,
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title,
-    description,
-  },
 };
 
 export default function RootLayout({
